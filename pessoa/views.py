f9bc7f710f582160a31d758864b5c1260bc32590
vinnytrevis <viniciustrevisan@gmail.com>
2022-08-22 15:15:04 -0300

    Criação da Edição, Exclusão e Filtro por nome

--- pessoa/views.py
+++ pessoa/views.py
@@ -1,5 +1,6 @@
+from ast import Delete
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .models import Pessoa
 from .forms import PessoaForm
 
@@ -7,7 +8,24 @@ class ListaPessoaView(ListView):
     model = Pessoa
     queryset = Pessoa.objects.all().order_by('nome_completo')
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        filtro_nome = self.request.GET.get('nome') or None
+
+        if filtro_nome:
+            queryset = queryset.filter(nome_completo__contains=filtro_nome)
+        return queryset
+
 class PessoaCreateView(CreateView):
     model = Pessoa
     form_class = PessoaForm
+    success_url = '/pessoas/'
+
+class PessoaUpdateView(UpdateView):
+    model = Pessoa
+    form_class = PessoaForm
+    success_url = '/pessoas/'
+
+class PessoaDeleteView(DeleteView):
+    model = Pessoa
     success_url = '/pessoas/'
