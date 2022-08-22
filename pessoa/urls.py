4a1d8235ac8909eb611449fa49f93c59b16c83cc
vinnytrevis <viniciustrevisan@gmail.com>
2022-08-22 18:02:44 -0300

    Parte 9

--- pessoa/urls.py
+++ pessoa/urls.py
@@ -1,17 +1,21 @@
 from django.urls import path
 from .views import ListaPessoaView, PessoaCreateView, PessoaUpdateView, PessoaDeleteView
 from . import views
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    path('', ListaPessoaView.as_view(), name='pessoa.index'),
-    path('novo/', PessoaCreateView.as_view(), name='pessoa.novo'),
-    path('<int:pk>/editar', PessoaUpdateView.as_view(), name='pessoa.editar'),
-    path('<int:pk>/remover', PessoaDeleteView.as_view(), name='pessoa.remover'),
-    path('<int:pk_pessoa>/contatos', views.contatos, name='pessoa.contatos'),
-    path('<int:pk_pessoa>/contato/novo/', views.contato_novo, name='contato.novo'),
-    path('<int:pk_pessoa>/contato/<int:pk>/editar/', views.contato_editar, name='contato.editar'),
-    path('<int:pk_pessoa>/contato/<int:pk>/remover', views.contato_remover, name='contato.remover')
-
-
+    path('', login_required(ListaPessoaView.as_view()), name='pessoa.index'),
+    path('novo/', login_required(PessoaCreateView.as_view()), name='pessoa.novo'),
+    path('<int:pk>/editar', login_required(PessoaUpdateView.as_view()),
+         name='pessoa.editar'),
+    path('<int:pk>/remover', login_required(PessoaDeleteView.as_view()),
+         name='pessoa.remover'),
+    path('<int:pk_pessoa>/contatos',
+         login_required(views.contatos), name='pessoa.contatos'),
+    path('<int:pk_pessoa>/contato/novo/',
+         login_required(views.contato_novo), name='contato.novo'),
+    path('<int:pk_pessoa>/contato/<int:pk>/editar',
+         login_required(views.contato_editar), name='contato.editar'),
+    path('<int:pk_pessoa>/contato/<int:pk>/remover',
+         login_required(views.contato_remover), name='contato.remover'),
 ]
-
--- project/urls.py
+++ project/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('pessoas/', include('pessoa.urls'))
+    path('pessoas/', include('pessoa.urls')),
+    path('accounts/', include('django.contrib.auth.urls'))
 
 ]
