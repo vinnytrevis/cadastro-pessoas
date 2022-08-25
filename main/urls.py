fb8eb78cc1bf80947cd32cf83650672a52f24fa4
vinnytrevis <viniciustrevisan@gmail.com>
2022-08-25 11:00:40 -0300

    Criação do Registro de Usuário

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, register
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    path('', login_required(HomeView.as_view()), name='home')
+    path('', login_required(HomeView.as_view()), name='home'),
+    path('accounts/register', register, name='register')
 ]
